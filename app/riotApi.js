baa8ffb24f2537fda8dc6658649aec9fab15c8af
Zelle97 <zeller.fabian.97@web.de>
2017-01-20 23:53:32 +0100

    RiotApi
    Get Divison with the summoner id

--- app/riotApi.js
+++ app/riotApi.js
@@ -3,14 +3,44 @@ var request = require('request');
 var summonerName = 'skumbagzelle';
 
 
+var summonerId = getId(summonerName,callbackId);
+var summonerDiv= getDivision(summonerId,callbackDiv);
 
 
 
-makeCall(process);
-console.log("Instant");
 
 //Call der mir daten liefert
-function makeCall (callback){
+function getDivision (summonerId, callback){
+  request.get(
+      'https://euw.api.pvp.net/api/lol/euw/v2.5/league/by-summoner/48918098/entry?api_key=RGAPI-b74a4bbe-b4bc-47d3-9943-4d8c8b3bcf15',
+      { json: { key: 'value' } },
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              s = summonerId;
+              console.log(body.name);
+              summonerDiv = body;
+
+          }
+          if (error) {
+              summonerDiv = error;
+          }
+         callbackDiv(summonerDiv);
+
+       }
+  );
+}
+//daten werden in der process methode verarbeitet
+function callbackDiv(summonerDiv){
+  console.log(summonerDiv);
+  return summonerDiv;
+}
+
+
+
+
+
+//Call der mir daten liefert
+function getId (summonerName, callback){
   request.get(
       'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+summonerName+'?api_key=RGAPI-b74a4bbe-b4bc-47d3-9943-4d8c8b3bcf15',
       { json: { key: 'value' } },
@@ -23,12 +53,13 @@ function makeCall (callback){
           if (error) {
               summonerId = error;
           }
-          callback(summonerId);
-      }
+         callbackId(summonerId);
+
+       }
   );
 }
-
 //daten werden in der process methode verarbeitet
-function process(summonerId){
+function callbackId(summonerId){
   console.log(summonerId);
+  return summonerId;
 }
