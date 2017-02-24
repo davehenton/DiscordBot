377e6bd3571a419c388bc2f86a1498affa8e07dc
Zelle97 <zeller.fabian.97@web.de>
2017-02-24 18:12:00 +0100

    Test changes
    deleted the starter test
    added chai assertion library
    better testing for time function

--- test/test.js
+++ test/test.js
@@ -1,20 +1,12 @@
-
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-
-
+var assert = require('chai').assert;
+var expect = require('chai').expect;
 var general = require('../app/general');
+
 describe('General', function(){
   describe('#time()', function(){
-    it('should display the time',function(){
+    it('should return the time as a String',function(){
       var time = general.basicFunctions.getTime()
-      assert.ok(time,'some value')
+      expect(time).to.be.a('string');
     })
   })
 })
